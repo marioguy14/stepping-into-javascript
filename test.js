04f81eb1de18defdae6b6a93ed859de0dc8e7c08
marioguy14 <54957654+marioguy14@users.noreply.github.com>
2020-02-04 17:50:57 -0500

    Updated with logical operators

--- test.js
+++ test.js
@@ -1,15 +1,8 @@
-var myAge = 49;
+var myAge = 26;
 
-if (myAge > 30)
-{
-	document.write("You are over 30!");
-} else if (myAge > 20){
-	
-	document.write("You are over 20!");
-}else if (myAge > 10){
-	
-	document.write("You are over 10!");
-}else{
-	
-	document.write("You are not over 10!");
+if (myAge < 18 || myAge > 30 || myAge === 25){
+	document.write("You aint coming");
+} else{
+	document.write("You can come, you cool dude");
 }
+		
